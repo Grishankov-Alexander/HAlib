46419c91a33ecc01899d907309fd56edecd390b9
Alexander Grishankov <grishankov.alexander@gmail.com>
2022-01-19 18:23:30 +0300

    Add License

--- HAlib.h
+++ HAlib.h
@@ -1,5 +1,42 @@
+/*************************************************************************
+ * HAlib
+ * Utilities library
+ *------------------------------------------------------------------------
+ * Copyright (c) 2022 Alexander Grishankov
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would
+ *    be appreciated but is not required.
+ *
+ * 2. Altered source versions must be plainly marked as such, and must not
+ *    be misrepresented as being the original software.
+ *
+ * 3. This notice may not be removed or altered from any source
+ *    distribution.
+ *
+ *************************************************************************/
+
+
+
+
+
+
 #ifndef HALIB_H
     #define HALIB_H
+
+    /* Disable symbol name mangling when using C++ compiler */
+    #ifdef __cplusplus
+        extern "C" {
+    #endif
 
     #include <stdio.h>
 
@@ -16,10 +53,7 @@
         #define HALIB_API
     #endif /* #ifdef _WIN32 */
 
-    /* Disable symbol name mangling when using C++ compiler */
-    #ifdef __cplusplus
-        extern "C" {
-    #endif
+
 
 
 
